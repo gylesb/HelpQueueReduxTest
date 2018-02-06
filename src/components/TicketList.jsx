95997aa03f25cfc4f6645d47e756eb59d0d74c13
Gyles Batara <gyles.batara@live.com>
2018-02-06 09:53:11 -0800

    Add eslint parameters and started call functions for the ticket list

--- src/components/TicketList.jsx
+++ src/components/TicketList.jsx
@@ -1,12 +1,34 @@
-import React from "react";
-import Ticket from "./Ticket";
+import React from 'react';
+import Ticket from './Ticket';
+
+var masterTicketList = [
+  {
+    names: 'Thato and Haley',
+    location: '3A',
+    issue: 'Firebase will not save on record. HALP'
+  },
+  {
+    names: 'Gyles and Batara',
+    location: '4B',
+    issue: 'Yes another weird error'
+  },
+  {
+    names: 'Epicodus and Coding Dojo',
+    location: 'Seattle',
+    issue: 'EPICODUS IS TAKING ALL OF MY STUDENTS'
+  }
+];
 
 function TicketList(){
   return (
-    <Ticket
-      location="3A"
-      names="Thato and Haley"
-      issue="Firebase will not save record!"/>
+    <div>
+      <hr/>
+      {masterTicketList.map((ticket, index) =>
+        <Ticket names={ticket.names}
+          location={ticket.location}
+          issue={ticket.issue} />
+      )}
+    </div>
   );
 }
 
