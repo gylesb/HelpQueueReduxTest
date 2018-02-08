47039322b9b7ca48c16d5b98317a6595af66b29b
Gyles Batara <gyles.batara@live.com>
2018-02-08 13:09:27 -0800

    App routing

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,10 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 function Header(){
   return (
     <h1>Help Queue!</h1>
+    <link to='/'>Home</link> | <Link to="/newticket">Create Ticket</Link>
   );
 }
 
