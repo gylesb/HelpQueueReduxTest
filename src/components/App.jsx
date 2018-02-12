625678c15540559edc9741ea388981768e8a3a8b
Gyles Batara <gyles.batara@live.com>
2018-02-12 11:15:51 -0800

    Updated route for ticket

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -4,6 +4,7 @@ import TicketList from './TicketList';
 import { Switch, Route } from 'react-router-dom';
 import NewTicketForm from './NewTicketForm';
 import Error404 from './Error404';
+import NewTicketControl from './NewTicketControl';
 
 function App(){
   return (
@@ -11,7 +12,7 @@ function App(){
       <Header/>
       <Switch>
         <Route exact path='/' component={TicketList} />
-        <Route path='/newticket' component={NewTicketForm} />
+        <Route path='/newticket' component={NewTicketControl} />
         <Route component = {Error404} />
       </Switch>
     </div>
