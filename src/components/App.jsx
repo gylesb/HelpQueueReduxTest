32a44ad7a22f943cc879dc9602ebb27d36a969fa
Gyles Batara <gyles.batara@live.com>
2018-02-13 10:36:29 -0800

    Declared App state and refactored App component into a class-based component

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,7 +2,6 @@ import React from 'react';
 import Header from './Header';
 import TicketList from './TicketList';
 import { Switch, Route } from 'react-router-dom';
-// import NewTicketForm from './NewTicketForm';
 import Error404 from './Error404';
 import NewTicketControl from './NewTicketControl';
 
@@ -18,4 +17,5 @@ function App(){
     </div>
   );
 }
+
 export default App;
--- src/components/TicketList.jsx
+++ src/components/TicketList.jsx
@@ -1,23 +1,23 @@
 import React from 'react';
 import Ticket from './Ticket';
 
-var masterTicketList = [
-  {
-    names: 'Thato and Haley',
-    location: '3A',
-    issue: 'Firebase will not save on record. HALP'
-  },
-  {
-    names: 'Gyles and Batara',
-    location: '4B',
-    issue: 'Yes another weird error'
-  },
-  {
-    names: 'Epicodus and Coding Dojo',
-    location: 'Seattle',
-    issue: 'EPICODUS IS TAKING ALL OF MY STUDENTS'
-  }
-];
+// var masterTicketList = [
+//   {
+//     names: 'Thato and Haley',
+//     location: '3A',
+//     issue: 'Firebase will not save on record. HALP'
+//   },
+//   {
+//     names: 'Gyles and Batara',
+//     location: '4B',
+//     issue: 'Yes another weird error'
+//   },
+//   {
+//     names: 'Epicodus and Coding Dojo',
+//     location: 'Seattle',
+//     issue: 'EPICODUS IS TAKING ALL OF MY STUDENTS'
+//   }
+// ];
 
 function TicketList(){
   return (
